746139f340d070d04ab199b8b1f458b68c6f41d1
Adittt17 <adityopangestu01@gmail.com>
2026-07-04 22:49:11 +0700

    Add generated folder

--- services/document_service.py
+++ services/document_service.py
@@ -4,6 +4,10 @@ from pathlib import Path
 from utils.word_generator import generate_word
 from utils.date_formatter import format_date_indonesia
 
+# Pastikan folder generated selalu ada
+OUTPUT_DIR = Path("generated")
+OUTPUT_DIR.mkdir(parents=True, exist_ok=True)
+
 
 def create_document(
     template_name,
@@ -12,34 +16,24 @@ def create_document(
 ):
 
     templates = {
-
         "Surat Mutasi": "templates/surat mutasi.docx",
-
         "Surat Promosi": "templates/surat promosi.docx",
-
         "Surat Keterangan Kerja": "templates/surat keterangan kerja.docx"
-
     }
 
-    template_path = templates[template_name]
+    template_path = Path(templates[template_name])
 
     filename = (
-        employee["employee_id"]
-        + "_"
-        + datetime.now().strftime("%Y%m%d_%H%M%S")
-        + ".docx"
+        f"{employee['employee_id']}_"
+        f"{datetime.now().strftime('%Y%m%d_%H%M%S')}.docx"
     )
 
-    output_path = Path("generated") / filename
+    output_path = OUTPUT_DIR / filename
 
     data = {
-
         **employee,
-
         **form_data,
-
         "today": format_date_indonesia(datetime.now().date())
-
     }
 
     generate_word(
